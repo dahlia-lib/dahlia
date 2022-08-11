462fc865d4fd72d125bba096cff964e3d709b0b8
trag1c <77130613+trag1c@users.noreply.github.com>
2022-08-11 15:09:22 +0200

    Updated __all__

--- src/dahlia/__init__.py
+++ src/dahlia/__init__.py
@@ -1,3 +1,3 @@
-from .dahlia import clean, clean_ansi, config, test, dahlia, dinput, dprint
+from .dahlia import clean, clean_ansi, config, reset, test, dahlia, dinput, dprint
 
-__all__ = ("clean", "clean_ansi", "config", "test", "dahlia", "dinput", "dprint")
+__all__ = ("clean", "clean_ansi", "config", "reset", "test", "dahlia", "dinput", "dprint")
