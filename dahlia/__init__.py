1d43939b1b023d89b375b4e043f30354351d3281
trag1c <trag1cdev@yahoo.com>
2024-05-21 13:14:21 +0200

    Sorted `__all__`

--- dahlia/__init__.py
+++ dahlia/__init__.py
@@ -2,8 +2,8 @@ from .dahlia import Dahlia, Depth
 from .utils import clean, clean_ansi
 
 __all__ = (
-    "clean",
-    "clean_ansi",
     "Dahlia",
     "Depth",
+    "clean",
+    "clean_ansi",
 )
