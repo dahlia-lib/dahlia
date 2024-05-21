f5d02a8974f16cb30ddc8371e645bb01a08b2ce9
trag1c <trag1cdev@yahoo.com>
2024-05-21 13:15:06 +0200

    Removed legacy functions (#18)

--- dahlia/__init__.py
+++ dahlia/__init__.py
@@ -1,13 +1,9 @@
 from .dahlia import Dahlia, Depth
-from .legacy import dahlia, dinput, dprint
 from .utils import clean, clean_ansi
 
 __all__ = (
-    "clean",
-    "clean_ansi",
     "Dahlia",
     "Depth",
-    "dahlia",
-    "dinput",
-    "dprint",
+    "clean",
+    "clean_ansi",
 )
