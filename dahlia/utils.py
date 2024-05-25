aa4623869c84c4704abf6b8e3b69c58d8798b4cd
trag1c <trag1cdev@yahoo.com>
2024-05-25 15:59:10 +0200

    Improved convert/clean/clean_ansi perf by deduplicating replacements

--- dahlia/utils.py
+++ dahlia/utils.py
@@ -1,42 +1,48 @@
 from __future__ import annotations
 
 import re
+from typing import TYPE_CHECKING
 
 from dahlia.constants import ANSI_REGEX, CODE_REGEXES, NO_GROUP_CODES
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
 
 
 def clean(string: str, marker: str = "&") -> str:
     """Removes all Dahlia codes from a string."""
-    for code, *_ in reversed(_find_codes(string, _with_marker(marker))):
-        string = string.replace(code, marker if code[1] == "_" else "", 1)
+    for code, *_ in _find_codes(string, _with_marker(marker)):
+        string = string.replace(code, marker if code[1] == "_" else "")
     return string
 
 
 def clean_ansi(string: str) -> str:
     """Removes all ANSI codes from a string."""
-    for ansi_code in _find_ansi_codes(string):
-        string = string.replace(ansi_code, "", 1)
+    for ansi_code in set(_find_ansi_codes(string)):
+        string = string.replace(ansi_code, "")
     return string
 
 
 def _find_codes(
     string: str, patterns: list[re.Pattern[str]]
-) -> list[tuple[str, bool | None, str]]:
-    return [
-        (match[0], None, match[1])  # full-reset, escape
-        if (full_snd_char := match[0][1]) in NO_GROUP_CODES
-        else (
-            (match[0], None, match[1])  # reset
-            if full_snd_char == "r"
-            else (match[0], match[1] == "~", match[2])  # regular
+) -> Iterator[tuple[str, bool | None, str]]:
+    return reversed(
+        dict.fromkeys(
+            (match[0], None, match[1])  # full-reset, escape
+            if (full_snd_char := match[0][1]) in NO_GROUP_CODES
+            else (
+                (match[0], None, match[1])  # reset
+                if full_snd_char == "r"
+                else (match[0], match[1] == "~", match[2])  # regular
+            )
+            for pattern in patterns
+            for match in pattern.finditer(string)
         )
-        for pattern in patterns
-        for match in pattern.finditer(string)
-    ]
+    )
 
 
-def _find_ansi_codes(string: str) -> list[str]:
-    return [match.group(0) for match in ANSI_REGEX.finditer(string)]
+def _find_ansi_codes(string: str) -> set[str]:
+    return {match.group(0) for match in ANSI_REGEX.finditer(string)}
 
 
 def _with_marker(marker: str) -> list[re.Pattern[str]]:
